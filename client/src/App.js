c703176999ede35bc528f8f5df10b7f50505d87f
Lambda_School_Loaner_145 <lambda_school_loaner_145@Lambda-School-Loaner-145s-MacBook-Pro.local>
2019-09-06 09:47:12 -0700

    finally rendering something

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,35 @@
 import React from 'react';
-import PlayerList from './components/PlayerList.js';
+import { PlayerList } from './components/PlayerList.js';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <PlayerList />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      players: {},
+      location: "",
+      rank: ""
+    };
+  }
+
+  componentDidMount() {
+    axios.get("https://googletrends.github.io/data/")
+        .then(res => {
+            console.log('api res: ', res)
+        })
+        .catch(err => {
+            console.log('api error: ', err)
+        })
+}
+
+  render() {
+    return (
+      <div className="App">
+        <PlayerList />
+      </div>
+    );
+  }
 }
 
 export default App;
--- client/src/components/PlayerList.js
+++ client/src/components/PlayerList.js
@@ -1,21 +1,14 @@
 import React from 'react';
+import { useLocalStorage } from '../hooks/useLocalStorage';
 import PlayerCard from './PlayerCard.js';
 
-class PlayerList extends React.Component {
-    
-    componentDidMount() {
-        axios.get("https://googletrends.github.io/data/")
-            .then(res => {
-                console.log('api res: ', res)
-            })
-            .catch(err => {
-                console.log('api error: ', err)
-            })
-    }
+export const PlayerList = (initialValue) => {
+    const [players, setPlayers] = useLocalStorage('players', initialValue)
 
-    render(
+    
+    
         return( 
             <PlayerCard />
         )
-    )
+    
 }
