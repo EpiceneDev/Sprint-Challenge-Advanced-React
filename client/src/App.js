3dd6212047068fa8b9e2febff4abb77ef1f0580d
Lambda_School_Loaner_145 <lambda_school_loaner_145@Lambda-School-Loaner-145s-MacBook-Pro.local>
2019-09-06 09:32:05 -0700

    useLocalStorage in, not sure how to use it

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import PlayerList from './components/PlayerList.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+      <PlayerList />
     </div>
   );
 }
---
+++ client/src/components/PlayerList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PlayerCard from './PlayerCard.js';
+
+class PlayerList extends React.Component {
+
+    componentDidMount() {
+        axios.get("https://googletrends.github.io/data/")
+            .then(res => {
+                console.log('api res: ', res)
+            })
+            .catch(err => {
+                console.log('api error: ', err)
+            })
+    }
+
+    render(
+        return( 
+            <PlayerCard />
+        )
+    )
+}
