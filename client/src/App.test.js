b3f2c3151ed305bf0ae518d2c0223b8197224d29
Lambda_School_Loaner_145 <lambda_school_loaner_145@Lambda-School-Loaner-145s-MacBook-Pro.local>
2019-09-06 14:47:40 -0700

    worked on testing

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -4,7 +4,20 @@ import ReactDOM from 'react-dom';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('search for strike', () => {
+test('search for country', () => {
   const wrapper = render(<App />);
-  wrapper.getAllByText(/player/i);
+  wrapper.getAllByText(/country/i);
 });
+
+//*****HAD ISSUES WITH JEST  */
+
+// it('renders without crashing', () => {
+//   const wrapper = rtl.render(
+//     <span className="greet">hello world</span>
+//   );
+//   const element = wrapper.queryByText(/hello/i);
+
+//     expect(element).toBeTruthy(); // jest matcher
+//     expect(element).toBeInTheDocument(); // jest-dom matcher
+//     expect(element).toBeVisible(); // 
+// });
--- client/src/components/PlayerCard.js
+++ client/src/components/PlayerCard.js
@@ -1,22 +1,26 @@
 import React from 'react';
 
 const PlayerCard = (props) => {
-    console.log("playercard props: ", props.players);
     const info = [props.players];
+    console.log("playercard info: ", info);
+
     return (
         <>
-        {info.map(player => {
+            {info.map((player, key) => {
+                console.log("player ", player)
+            
+                
                 return (
-                    <div className="player-card" key={Date.now}>
+                    <div className="player-card" key={player.id}>
                         <h2>Hello, my name is {player.name}.</h2>
                         <h3>I play ball for {player.country}.</h3>
                         <p>I am ranked number {player.searches} in online searches.</p>
                     </div>
                 )
-        })}
-        <h1>hello</h1>
+            })}
         </>
-    )
+    )   
 }
+            
 
 export default PlayerCard;
